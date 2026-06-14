3c95affbca90973142e1fc0ddf21694c193a0a00
Agmish <Ahmad@mishkhas.com>
2026-06-14 21:30:34 +0300

    Fit Inheritance background with layout ratio

--- web-sdk/apps/the-inheritance/src/components/Background.svelte
+++ web-sdk/apps/the-inheritance/src/components/Background.svelte
@@ -4,16 +4,9 @@
 	import { getContext } from '../game/context';
 
 	const context = getContext();
+	const backgroundProps = $derived(context.stateLayoutDerived.normalBackgroundLayout({ scale: 1 }));
 </script>
 
 <Rectangle {...context.stateLayoutDerived.canvasSizes()} backgroundColor={0x000000} zIndex={-3} />
 
-<Sprite
-	key="inheritanceBackground"
-	anchor={0.5}
-	x={context.stateLayoutDerived.canvasSizes().width / 2}
-	y={context.stateLayoutDerived.canvasSizes().height / 2}
-	width={context.stateLayoutDerived.canvasSizes().width}
-	height={context.stateLayoutDerived.canvasSizes().height}
-	zIndex={-2}
-/>
+<Sprite key="inheritanceBackground" {...backgroundProps} zIndex={-2} />
