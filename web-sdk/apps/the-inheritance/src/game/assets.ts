a49697414d8fe83615764b7680d9b6c230c34a54
Agmish <Ahmad@mishkhas.com>
2026-06-19 22:49:01 +0300

    Use frameQ PNG asset

--- web-sdk/apps/the-inheritance/src/game/assets.ts
+++ web-sdk/apps/the-inheritance/src/game/assets.ts
@@ -23,7 +23,7 @@ export default {
 	},
 	inheritanceFrame: {
 		type: 'sprite',
-		src: inheritanceAsset('ui/frame.svg'),
+		src: inheritanceAsset('ui/frameQ.png'),
 		preload: true,
 	},
 	inheritanceUiPanel: {
