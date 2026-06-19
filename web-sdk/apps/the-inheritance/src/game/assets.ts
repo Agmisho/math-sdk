f48b07989f0e9a1695c596066fa891185c0d38f1
Agmish <Ahmad@mishkhas.com>
2026-06-19 19:57:47 +0300

    Revert frame asset to PNG

--- web-sdk/apps/the-inheritance/src/game/assets.ts
+++ web-sdk/apps/the-inheritance/src/game/assets.ts
@@ -23,7 +23,7 @@ export default {
 	},
 	inheritanceFrame: {
 		type: 'sprite',
-		src: inheritanceAsset('ui/frame.svg'),
+		src: inheritanceAsset('ui/frame.png'),
 		preload: true,
 	},
 	inheritanceUiPanel: {
