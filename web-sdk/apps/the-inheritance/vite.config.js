fe1cff6f3cea9da45ca5de65d0c2eddaa448ba54
Agmish <Ahmad@mishkhas.com>
2026-06-23 10:20:25 +0300

    Fix inheritance Vite dev prebundling

--- web-sdk/apps/the-inheritance/vite.config.js
+++ web-sdk/apps/the-inheritance/vite.config.js
@@ -1,4 +1,50 @@
 // @ts-ignore
 import config from 'config-vite';
 
-export default config();
+const workspacePackages = [
+	'components-layout',
+	'components-pixi',
+	'components-shared',
+	'components-ui-html',
+	'components-ui-pixi',
+	'constants-shared',
+	'envs',
+	'pixi-svelte',
+	'rgs-fetcher',
+	'rgs-requests',
+	'state-shared',
+	'utils-bet',
+	'utils-book',
+	'utils-event-emitter',
+	'utils-fetcher',
+	'utils-layout',
+	'utils-shared',
+	'utils-slots',
+	'utils-sound',
+	'utils-xstate',
+];
+
+export default () => {
+	const baseConfig = config();
+
+	return {
+		...baseConfig,
+		optimizeDeps: {
+			...baseConfig.optimizeDeps,
+			include: [
+				...(baseConfig.optimizeDeps?.include ?? []),
+				'@lingui/core',
+				'@lingui/message-utils/compileMessage',
+				'@messageformat/parser',
+				'earcut',
+				'eventemitter3',
+				'howler',
+				'lodash',
+				'pixi.js',
+				'resize-observer',
+				'webfontloader',
+			],
+			exclude: [...(baseConfig.optimizeDeps?.exclude ?? []), ...workspacePackages],
+		},
+	};
+};
