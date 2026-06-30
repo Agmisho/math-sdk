fa2c84a8bdd5a07a51279e1dd0f037f7dd287ddd
Agmish <Ahmad@mishkhas.com>
2026-07-01 00:08:16 +0300

    Restore shared Svelte configuration to official defaults

--- web-sdk/packages/config-svelte/index.js
+++ web-sdk/packages/config-svelte/index.js
@@ -1,11 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isTheInheritanceApp = process.cwd().replaceAll('\\', '/').endsWith('/apps/the-inheritance');
-const releaseBuildDir = isTheInheritanceApp
-	? process.env.THE_INHERITANCE_RELEASE_BUILD_DIR
-	: undefined;
-
 /** @type {import('@sveltejs/kit').Config} */
 export default () => ({
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -13,14 +8,7 @@ export default () => ({
 	preprocess: vitePreprocess(),
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(
-			releaseBuildDir
-				? {
-						pages: releaseBuildDir,
-						assets: releaseBuildDir,
-					}
-				: undefined,
-		),
+		adapter: adapter(),
 		output: {
 			bundleStrategy: 'inline',
 		},
